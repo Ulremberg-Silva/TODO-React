48cf570fb9504acbe46558d03f96272393e13e8c
ulremberg-silva <ulremberg.silva@dtidigital.com.br>
2022-10-24 12:02:48 -0300

    corrigido validacao ao criar tarefa

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -2,7 +2,7 @@ import React, { useState, FormEvent } from "react";
 import TodoForm from "../../components/TodoForm/TodoForm";
 import TodoList from "../../components/TodoList/TodoList";
 import { ITask } from "../../interface/ITask";
-import './style.css';
+import "./style.css";
 
 export function HomePage() {
   const [todos, setTodos] = useState<ITask[]>([]);
@@ -12,20 +12,23 @@ export function HomePage() {
   function handleSubmit(e: FormEvent) {
     e.preventDefault();
 
-    const createTodoItem = {
-      id: new Date().getTime().toString(),
-      name: todo,
-    };
+    if (todo.trim().length !== 0) {
+      const createTodoItem = {
+        id: new Date().getTime().toString(),
+        name: todo,
+      };
 
-    setTodos([...todos].concat(createTodoItem));
+      setTodos([...todos].concat(createTodoItem));
+      setTodo("");
+    }
+
     setTodo("");
-  };
+  }
 
-  
   return (
     <div className="todo-form">
       <h1 className="head-task">Task</h1>
-      <p>You have {todos.length} Todos </p>      
+      <p>You have {todos.length} Todos </p>
       <TodoList todos={todos} setTodos={setTodos} />
       <TodoForm todo={todo} setTodo={setTodo} handleAdd={handleSubmit} />
     </div>
